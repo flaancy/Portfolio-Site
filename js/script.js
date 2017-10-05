96b1c8cff85f3a0e84a4e649c6bb72d525d7ee8f
flaancy <flaviobarbosa42@icloud.com>
2017-10-05 20:08:13 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+function openPage(pageName, elmnt, color) {
+    var i, tabcontent, tablinks;
+    //esconde o conteudo aberto
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        //cleans javascript definitions like color 
+        tablinks[i].style.backgroundColor = "";
+        tablinks[i].style.color = "";
+    }
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.color = "white";
+    elmnt.style.backgroundColor = color; 
+}
